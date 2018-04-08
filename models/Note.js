fc68226ae0472016fa0a61187579490b52614598
Robin Bertuccelli <robin.bertuccelli@gmail.com>
2018-04-07 19:04:48 -0500

    a bit closer

--- models/Note.js
+++ models/Note.js
@@ -15,8 +15,7 @@ var NoteSchema = new Schema({
   }
 });
 
-// Remember, Mongoose will automatically save the ObjectIds of the notes
-// These ids are referred to in the Article model
+
 
 // Create the Note model with the NoteSchema
 var Note = mongoose.model("Note", NoteSchema);
